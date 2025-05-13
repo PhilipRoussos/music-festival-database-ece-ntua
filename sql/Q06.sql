26bc990fa4ef2e5c23a22e123ee794aae2a8b894
alexp9904 <117583149+alexp9904@users.noreply.github.com>
2025-05-13 14:51:46 +0300

    Update Q06.sql

--- sql/Q06.sql
+++ sql/Q06.sql
@@ -9,7 +9,7 @@ JOIN festival_event FE ON T.event_id = FE.event_id
 JOIN rating R ON T.IAN_number = R.IAN_number
 JOIN performance P ON R.performance_id = P.performance_id 
 WHERE
-    T.attendee_id = 1  
+    T.attendee_id = 3  
     AND T.activated = TRUE              
     AND P.event_id = FE.event_id         
 GROUP BY
